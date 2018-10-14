af4683f84e1e065cfc6bb7a0b90781c81e2b2251
zlarkisz <blefgufin@gmail.com>
2018-10-14 17:28:41 +0300

    semalt

---
+++ @media/js/app.js
@@ -0,0 +1,29 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        title: 'My Todo List',
+        newTodo: '',
+        todos: []
+    },
+
+    methods: {
+        addTodo () {
+            this.todos.push({
+                title: this.newTodo,
+                done: false
+            });
+            this.newTodo= '';
+        },
+
+        removeTodo (todo) {
+            const todoIndex = this.todos.indexOf(todo);
+            this.todos.splice(todoIndex, 1);
+        },
+
+        allDone () {
+            this.todos.forEach(todo => {
+                todo.done = true;
+            });
+        }
+    }
+});
